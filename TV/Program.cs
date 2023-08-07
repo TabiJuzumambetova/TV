95abcf9653577296591ab0b2b22c7e078de16f04
Tabi Juzumambetova <tabihat.juzumambetova.ph@gmail.com>
2023-08-07 17:31:37 +0300

    Добавьте файлы проекта.

---
+++ TV/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int channelLimit = 20; 
+            TV tv = new TV(1, channelLimit);
+
+            Console.WriteLine("Добро пожаловать в имитацию работы телевизора!");
+
+            while (true)
+            {
+                tv.ShowCurrentChannel();
+
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1 - Переключить на следующий канал");
+                Console.WriteLine("2 - Переключить на предыдущий канал");
+                Console.WriteLine("3 - Перейти к каналу по номеру");
+                Console.WriteLine("0 - Выход");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Пожалуйста, введите число от 0 до 3.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Программа завершена.");
+                        return;
+                    case 1:
+                        tv.NextChannel();
+                        break;
+                    case 2:
+                        tv.PreviousChannel();
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите номер канала:");
+                        int channelNumber;
+                        if (int.TryParse(Console.ReadLine(), out channelNumber))
+                        {
+                            tv.MoveToChannel(channelNumber);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите число от 1 до " + channelLimit);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Пожалуйста, выберите от 0 до 3.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ TV/TV.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV
+{
+    class TV
+    {
+        private int currentChannel;
+        private int channelLimit;
+
+        public TV(int currentChannel, int channelLimit)
+        {
+            this.currentChannel = currentChannel;
+            this.channelLimit = channelLimit;
+        }
+
+        public void NextChannel()
+        {
+            currentChannel = (currentChannel % channelLimit) + 1;
+        }
+
+        public void PreviousChannel()
+        {
+            currentChannel = (currentChannel - 2 + channelLimit) % channelLimit + 1;
+        }
+
+        public void MoveToChannel(int channel)
+        {
+            if (channel > 0 && channel <= channelLimit)
+            {
+                currentChannel = channel;
+            }
+            else
+            {
+                Console.WriteLine("Канал с указанным номером не существует.");
+            }
+        }
+
+        public void ShowCurrentChannel()
+        {
+            Console.WriteLine("Текущий канал: " + currentChannel);
+        }
+    }
+}
